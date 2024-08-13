061c0a2de5561845406d5467fa8971c2c8255894
Bryan Solares <solares.josue@outlook.com>
2024-08-12 21:32:38 -0600

    Add github action and changes of lambda code

--- lambda/main.py
+++ lambda/main.py
@@ -1,5 +1,13 @@
+import json
+
 def handler(event, context):
+
+  response_body = {
+    "message": "Hello World!",
+    "version": "1.0.0"
+  }
+
   return {
     "statusCode": 200,
-    "body": "Hello, CDK!"
+    "body": json.dumps(response_body)
   }
